5369fb8c3f7e0a235953993024d88efc4031de3e
unknown <Md Forhad Hossain>
2023-07-04 15:34:30 +0800

    4th commit

--- AccessTimber/Program.cs
+++ AccessTimber/Program.cs
@@ -10,6 +10,21 @@ builder.Services.AddScoped<IDapper, Dapperr>();
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+var PMSAllowOrigin = "_myPMSOrigins";
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: PMSAllowOrigin,
+        builder =>
+        {
+            builder.WithOrigins("http://localhost:3000", "http://192.168.13.103:3000", "http://192.168.10.37:3000",
+                                "https://apps.bitopibd.com/PMSLive/", "https://apps.bitopibd.com/PMS/", "*")
+                                .AllowAnyHeader()
+                                .AllowAnyMethod();
+        });
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -23,10 +38,13 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseRouting(); // Enable routing
+
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
 app.MapControllers();
+
 
 app.Run();
