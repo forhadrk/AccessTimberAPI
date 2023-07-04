85109cd722b6f46f69501167ac6a5372aa339432
unknown <Md Forhad Hossain>
2023-07-04 15:45:41 +0800

    5th commit

--- AccessTimber/Controllers/ItemsController.cs
+++ AccessTimber/Controllers/ItemsController.cs
@@ -8,9 +8,8 @@ using System.Data;
 
 namespace AccessTimber.Controllers
 {
-    //[Route("api/items")]
+    [Route("api/items")]
     [ApiController]
-    [Route("[controller]")]
     public class ItemsController : ControllerBase
     {
         private readonly IDapper _dapper;
